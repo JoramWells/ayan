05837284b7a75e20676946480a7070ac79ef7fc5
bramuel <jorammanoah1@gmail.com>
2021-07-05 13:23:01 +0300

    Added header

--- js/index.js
+++ js/index.js
@@ -2,4 +2,9 @@ const navbar = document.querySelector('.navbar')
 window.onscroll = function(){
     var top = window.scrollY;
     console.log(top)
+    if(top > 450){
+        navbar.classList.add('active')
+    }else{
+        navbar.classList.remove('active')
+    }
 }
