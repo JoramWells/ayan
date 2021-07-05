fd8ec74a947b17f9f2c4f4adbe172bbb46ab04c4
bramuel <jorammanoah1@gmail.com>
2021-07-05 15:52:35 +0300

    Added imports

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ const navbar = document.querySelector('.navbar')
 const navItem = document.querySelector('.nav-item')
 window.onscroll = function(){
     var top = window.scrollY;
+    console.log(top)
     if(top > 500){
         navbar.classList.add('active')
         navItem.classList.add('active')
